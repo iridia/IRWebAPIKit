d06712ae4e10eea754a187f3591730aa5cdc6c54
Evadne Wu <ev@monoceroi.com>
2010-11-21 20:55:36 +0800

    ObjC | revises IRWebAPIResponseParser.h to fix several bugs and adds a simple parser for “key=value”-style responses

--- IRWebAPIResponseParser.h
+++ IRWebAPIResponseParser.h
@@ -16,7 +16,19 @@
 
 
 
+#pragma mark Parsers and Parser Bridges
+
+
+//	Parser blocks are supposed to be bridges to other larger, more capable Objective-C classes.
+//	i.e., you are not supposed to work an entire XML parser into a block.
+
+
+
+
+
 static inline IRWebAPIResponseParser IRWebAPIResponseDefaultParserMake () {
+
+//	Simply tucks the returned data into a dictionary
 
 	NSDictionary * (^defaultParser) (NSData *) = ^ NSDictionary * (NSData *inData) {
 	
@@ -24,7 +36,43 @@ static inline IRWebAPIResponseParser IRWebAPIResponseDefaultParserMake () {
 	
 	};
 
-	return defaultParser;
+	return [defaultParser retain];
+
+}
+
+
+
+
+
+static inline IRWebAPIResponseParser IRWebAPIResponseQueryResponseParserMake () {
+
+//	Parses UTF8 String Data Like:	
+//	
+//	Key=URL_Encoded_Value
+//	Another_Key=Another_Encoded_Value
+	
+	NSDictionary * (^queryResponseParser) (NSData *) = ^ NSDictionary * (NSData *inData) {
+	
+		if ([inData length] == 0)
+		return [NSDictionary dictionary];
+		
+		NSString *responseString = [[NSString alloc] initWithData:inData encoding:NSUTF8StringEncoding];
+
+		NSRegularExpression *expression = [NSRegularExpression regularExpressionWithPattern:@"(.+)=(.+)" options:NSRegularExpressionCaseInsensitive error:nil];
+		
+		NSMutableDictionary *returnedResponse = [NSMutableDictionary dictionary];
+		
+		[expression enumerateMatchesInString:responseString options:0 range:NSMakeRange(0, [responseString length]) usingBlock: ^ (NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
+		
+			[returnedResponse setObject:[responseString substringWithRange:[result rangeAtIndex:2]] forKey:[responseString substringWithRange:[result rangeAtIndex:1]]];
+		
+		}];
+		
+		return returnedResponse;
+	
+	};
+	
+	return [queryResponseParser retain];
 
 }
 
