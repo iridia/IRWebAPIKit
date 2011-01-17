016bfe396bd7b509e8747f1e76e42f3533ab8fe2
Evadne Wu <ev@monoceroi.com>
2011-01-17 17:10:56 +0800

    ObjC | adds unit tests for RFC 3986 compliant string encoding, HTML entities (as per the XHTML DTD) decoding

--- TestIRWebAPIKitStringHelpers.m
+++ TestIRWebAPIKitStringHelpers.m
@@ -13,9 +13,59 @@
 
 - (void) testHTMLEntitiesDecoding {
 
-	STAssertEqualObjects(IRWebAPIStringByDecodingXMLEntities(@"&lt;"), @"<", @"&lt; -> <");
+	NSDictionary *originalToDecoded = [NSDictionary dictionaryWithObjectsAndKeys:
 	
-//	(IRWebAPIStringByDecodingXMLEntities(@"&lt;"), @"")
+		@"<", @"&lt;",
+	
+	nil];
+
+	for (id aKey in originalToDecoded) {
+	
+		NSString *libraryDecoded = IRWebAPIStringByDecodingXMLEntities(aKey);
+	
+		STAssertEqualObjects(
+		
+			[originalToDecoded objectForKey:aKey], 
+			libraryDecoded,
+			@"Library-decoded string does not match the sample."
+			
+		);
+		
+	}
+
+}
+
+- (void) testRFC3986StringEncoding {
+
+//	Still at odds with this.
+	
+	NSDictionary *originalToEncoded = [NSDictionary dictionaryWithObjectsAndKeys:
+	
+		@"%25%21%24%26%27%28%29%2A%2B%2C%2F%3A%3B%3D%3F%40", @"%!$&'()*+,/:;=?@",
+	
+	nil];
+
+	for (id aKey in originalToEncoded) {
+	
+		NSString *libraryEncoded = IRWebAPIKitRFC3986EncodedStringMake(aKey);
+	
+		STAssertEqualObjects(
+		
+			[originalToEncoded objectForKey:aKey], 
+			libraryEncoded,
+			@"Library-encoded string does not match the sample."
+			
+		);
+		
+		STAssertEqualObjects(
+		
+			IRWebAPIKitRFC3986DecodedStringMake(libraryEncoded),
+			aKey,
+			@"Decoded library-encoded string does not match the sample."
+		
+		);
+		
+	}
 
 }
 
