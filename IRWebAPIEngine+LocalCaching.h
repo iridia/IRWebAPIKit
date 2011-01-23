25c9c45574ace8690d8eb7a85aba946d514b7277
Evadne Wu <ev@monoceroi.com>
2011-01-23 20:44:59 +0800

    ObjC | local file caching code WIP

---
+++ IRWebAPIEngine+LocalCaching.h
@@ -0,0 +1,16 @@
+//
+//  IRWebAPIEngine+LocalCaching.h
+//  IRWebAPIKit
+//
+//  Created by Evadne Wu on 1/23/11.
+//  Copyright 2011 Iridia Productions. All rights reserved.
+//
+
+#import "IRWebAPIEngine.h"
+
+
+@interface IRWebAPIEngine (LocalCaching)
+
+- (NSURL *) createEmptyTemporaryFileURL;
+
+@end
---
+++ IRWebAPIEngine+LocalCaching.m
@@ -0,0 +1,14 @@
+//
+//  IRWebAPIEngine+LocalCaching.m
+//  IRWebAPIKit
+//
+//  Created by Evadne Wu on 1/23/11.
+//  Copyright 2011 Iridia Productions. All rights reserved.
+//
+
+#import "IRWebAPIEngine+LocalCaching.h"
+
+
+@implementation IRWebAPIEngine (LocalCaching)
+
+@end
