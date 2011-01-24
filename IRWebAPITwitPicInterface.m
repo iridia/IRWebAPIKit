ee597aaffae5eba4478f390a81b046ea658bbbf6
Evadne Wu <ev@monoceroi.com>
2011-01-24 21:49:34 +0800

    ObjC | IRWebAPITwitPicInterface WIP

--- IRWebAPITwitPicInterface.m
+++ IRWebAPITwitPicInterface.m
@@ -11,20 +11,48 @@
 
 @implementation IRWebAPITwitPicInterface
 
+- (id) init {
+
+	IRWebAPIContext *twitPicContext = [[[IRWebAPIContext alloc] initWithBaseURL:[NSURL URLWithString:@"http://api.twitpic.com/"]] autorelease];
+	IRWebAPIEngine *twitPicEngine = [[[IRWebAPIEngine alloc] initWithContext:twitPicContext] autorelease];
+	
+	twitPicEngine.parser = IRWebAPIResponseDefaultJSONParserMake();
+		
+	self = [self initWithEngine:twitPicEngine authenticator:nil];
+	
+	[twitPicEngine.globalRequestPreTransformers addObject:[[twitPicEngine class] defaultFormMultipartTransformer]];
+	[twitPicEngine.globalResponsePostTransformers addObject:[[twitPicEngine class] defaultCleanUpTemporaryFilesResponseTransformer]];
+
+	if (!self) return nil;
+
+	return self;
+
+}
+
 - (void) uploadImageAtURL:(NSURL *)inImageURL onSuccess:(IRWebAPIInterfaceCallback)inSuccessCallback onFailure:(IRWebAPIInterfaceCallback)inFailureCallback {
 
-	if (!self.apiKey) {
-	
-		NSLog(@"%@: No API Key.", self);
-	
-		if (inFailureCallback)
-		inFailureCallback(nil, NO, NO);
-		
-		return;
-	
-	}
+	NSAssert(self.apiKey, @"%@ needs an API key.", self);
+	NSAssert(self.authenticatingInterface, @"%@ needs an authenticating interface so oAuth Echo works.", self);
 	
 	NSAssert(NO, @"Implement!");
+	
+	[self.engine fireAPIRequestNamed:@"test.php" withArguments:[NSDictionary dictionaryWithObjectsAndKeys:
+	
+		self.apiKey, @"key",
+	
+	nil] options:[NSDictionary dictionaryWithObjectsAndKeys:
+	
+		[NSDictionary dictionaryWithObjectsAndKeys:
+		
+			inImageURL, @"image",
+		
+		nil], kIRWebAPIEngineRequestContextFormMultipartFieldsKey,
+	
+	nil] successHandler: ^ (NSDictionary *inResponseOrNil, NSDictionary *inResponseContext, BOOL *outNotifyDelegate, BOOL *outShouldRetry) {
+	
+		NSLog(@"Success: %@", inResponseOrNil);
+	
+	} failureHandler:nil];
 
 }
 
