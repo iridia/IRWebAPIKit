iridia/IRWebAPIKit

commit e799a4d818d26161931785d41c1fbc6c33a4700f

ObjC | revises Twitter response validator generators

IRWebAPITwitterInterface+Validators.m
@@ -11,19 +11,90 @@
 
 @implementation IRWebAPITwitterInterface (Validators)
 
+- (IRWebAPIResposeValidator) defaultNoErrorValidator {
+
+	return [[(^ (NSDictionary *inResponseOrNil, NSDictionary *inResponseContext) {
+	
+		NSHTTPURLResponse *response = (NSHTTPURLResponse *)[inResponseContext objectForKey:kIRWebAPIEngineResponseContextURLResponseName];
+	
+		BOOL noError = ([response statusCode] == 200);
+	
+		if (!noError) {
+			
+			IRWebAPIKitLog(@"Error: %x %@", [response statusCode], [[response class] localizedStringForStatusCode:[response statusCode]]);
+			
+			if ([inResponseOrNil isEqual:[NSNull null]]) {
+				
+				return NO;
+				
+			}
+			
+			id errorContent = nil;
+			if ((errorContent = [inResponseOrNil valueForKeyPath:@"error"])) {
+				
+				IRWebAPIKitLog(@"Error from Server: %@", errorContent);
+				
+			}
+
+			return NO;
+		
+		}
+	
+		return YES;
+	
+	}) copy] autorelease];	
+
+}
+
+- (IRWebAPIResposeValidator) defaultValidatorForArrayNamed:(NSString *)inKeyPathToResponseArray withElementKeyPaths:(NSArray *)inKeyPaths validator:(BOOL(^)(id aKeyPath, id currentObject))inValidator {
+
+	return [[(^ (NSDictionary *inResponseOrNil, NSDictionary *inResponseContext) {
+	
+		if (![self defaultNoErrorValidator](inResponseOrNil, inResponseContext))
+		return NO;
+		
+		NSArray *responseArray = [inResponseOrNil valueForKeyPath:inKeyPathToResponseArray];
+		if (![responseArray isKindOfClass:[NSArray class]])
+		return NO;
+		
+		for (id anObject in responseArray)
+		for (id aKeyPath in inKeyPaths)
+		if (!inValidator(aKeyPath, [anObject valueForKeyPath:aKeyPath]))
+		return NO;
+		
+		return YES;
+	
+	}) copy] autorelease];
+
+}
+
+- (IRWebAPIResposeValidator) defaultValidatorForArrayNamed:(NSString *)inKeyPathToResponseArray withElementKeyPaths:(NSArray *)inKeyPaths {
+
+	return [self defaultValidatorForArrayNamed:inKeyPathToResponseArray withElementKeyPaths:inKeyPaths validator: ^ (id aKeyPath, id currentObject) {
+	
+		return (BOOL)(currentObject != nil);
+	
+	}];
+
+}
+
+
+- (IRWebAPIResposeValidator) defaultExistingValueValidatorForKeyPaths:(NSArray *)inKeyPaths {
+
+	return [self defaultValidatorForArrayNamed:@"response" withElementKeyPaths:inKeyPaths];
+
+}
+
 - (IRWebAPIResposeValidator) defaultTimelineValidator {
 
-	return [[(^ (NSDictionary *inResponseOrNil) {
+	return [[(^ (NSDictionary *inResponseOrNil, NSDictionary *inResponseContext) {
 	
+		if (![self defaultExistingValueValidatorForKeyPaths:[NSArray arrayWithObject:@"response"]])
+		return NO;
+
 		id response = [inResponseOrNil valueForKeyPath:@"response"];
 		
-		if (!response)
-		return NO;
-	
-		if ([response isEqual:[NSNull null]])
-		return NO;
-	
-		if (![response isKindOfClass:[NSArray class]])
+		if (!response || [response isEqual:[NSNull null]] || [response isKindOfClass:[NSArray class]])
 		return NO;
 		
 		if ([(NSArray *)response count] > 0)
@@ -38,8 +109,11 @@
 
 - (IRWebAPIResposeValidator) defaultListsValidator {
 
-	return [[(^ (NSDictionary *inResponseOrNil) {
+	return [[(^ (NSDictionary *inResponseOrNil, NSDictionary *inResponseContext) {
 		
+		if (![self defaultNoErrorValidator](inResponseOrNil, inResponseContext))
+		return NO;
+
 		id response = [inResponseOrNil valueForKeyPath:@"response"];
 		
 		if (!response)
@@ -63,7 +137,10 @@
 
 - (IRWebAPIResposeValidator) defaultSingleTweetValidator {
 
-	return [[(^ (NSDictionary *inResponseOrNil) {
+	return [[(^ (NSDictionary *inResponseOrNil, NSDictionary *inResponseContext) {
+	
+		if (![self defaultNoErrorValidator](inResponseOrNil, inResponseContext))
+		return NO;
 	
 		if (![inResponseOrNil valueForKeyPath:@"text"])
 		return NO;
@@ -71,27 +148,6 @@
 		return YES;
 	
 	}) copy] autorelease];
-
-}
-
-- (IRWebAPIResposeValidator) defaultNoErrorValidator {
-
-	return [[(^ (NSDictionary *inResponseOrNil) {
-	
-		if ([inResponseOrNil isEqual:[NSNull null]])
-		return NO;
-	
-		if ([inResponseOrNil valueForKeyPath:@"error"]) {
-		
-			NSLog(@"Fix Me: defaultNoErrorValidator: %@", [inResponseOrNil valueForKeyPath:@"error"]);
-			
-			return NO;
-		
-		}
-		
-		return YES;
-	
-	}) copy] autorelease];	
 
 }
 

IRWebAPITwitterInterface+Validators.h
@@ -10,9 +10,26 @@
 
 @interface IRWebAPITwitterInterface (Validators)
 
+- (IRWebAPIResposeValidator) defaultNoErrorValidator;
+
+//	Makes sure the response is 200 and does not come with an error
+
+
+- (IRWebAPIResposeValidator) defaultValidatorForArrayNamed:(NSString *)inKeyPathToResponseArray withElementKeyPaths:(NSArray *)inKeyPaths validator:(BOOL(^)(id aKeyPath, id currentObject))inValidator;
+
+- (IRWebAPIResposeValidator) defaultValidatorForArrayNamed:(NSString *)inKeyPathToResponseArray withElementKeyPaths:(NSArray *)inKeyPaths;
+
+//	Makes sure that the key path inKeyPathToResponseArray points to an NSArray, and each element in that array has values described in inKeyPaths
+//	The default validator is a “non-nil” validator.
+
+
+- (IRWebAPIResposeValidator) defaultExistingValueValidatorForKeyPaths:(NSArray *)inKeyPaths;
+
+//	return [self defaultValidatorForArrayNamed:@"response" withElementKeyPaths:inKeyPaths];
+
+
 - (IRWebAPIResposeValidator) defaultTimelineValidator;
 - (IRWebAPIResposeValidator) defaultSingleTweetValidator;
-- (IRWebAPIResposeValidator) defaultNoErrorValidator;
 
 - (IRWebAPIResposeValidator) defaultListsValidator;
 

commit c23a6be8480258231a870ec16e19c4344508f855

ObjC | fixes Twitter timeline validator regression in e799a4d8

IRWebAPITwitterInterface+Validators.m
@@ -94,7 +94,7 @@
 
 		id response = [inResponseOrNil valueForKeyPath:@"response"];
 		
-		if (!response || [response isEqual:[NSNull null]] || [response isKindOfClass:[NSArray class]])
+		if (!response || ![response isKindOfClass:[NSArray class]])
 		return NO;
 		
 		if ([(NSArray *)response count] > 0)