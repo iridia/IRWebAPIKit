0c2720d5c6839064627112697956d3d546e9508d
Evadne Wu <ev@monoceroi.com>
2012-02-18 18:40:55 +0800

    removes dead code

--- IRWebAPITwitterInterface+Geo.m
+++ IRWebAPITwitterInterface+Geo.m
@@ -20,8 +20,6 @@
 
 - (void) reverseGeocodeWithLocation:(CLLocation *)inLocation userinfo:(NSDictionary *)inUserInfo onSuccess:(IRWebAPICallback)inSuccessCallback onFailure:(IRWebAPICallback)inFailureCallback {
 
-//	Since Twitter’s implementation is still unreliable, we are using the ol’ reliable Google Maps API here
-
 	IRWebAPIContext *googleMapsContext = [[[IRWebAPIContext alloc] initWithBaseURL:[NSURL URLWithString:@"http://maps.googleapis.com/maps/api/"]] autorelease];
 	IRWebAPIEngine *googleMapsEngine = [[[IRWebAPIEngine alloc] initWithContext:googleMapsContext] autorelease];
 	googleMapsEngine.parser = IRWebAPIResponseDefaultJSONParserMake();
@@ -60,55 +58,7 @@
 		inFailureCallback(inResponseOrNil, inResponseContext, outNotifyDelegate, outShouldRetry);
 	
 	}];
-
-
-
-
-	return;
-
-	[self.engine fireAPIRequestNamed:@"/geo/reverse_geocode.json" withArguments:[NSDictionary dictionaryWithObjectsAndKeys:
-
-		[NSNumber numberWithFloat:inLocation.coordinate.latitude], @"lat",
-		[NSNumber numberWithFloat:inLocation.coordinate.longitude], @"long",
-//		inLocation.horizontalAccuracy, @"accuracy",
-		@"1000m", @"accuracy",
-		@"1", @"max_results",
-
-	nil] options:nil successHandler: ^ (NSDictionary *inResponseOrNil, NSDictionary *inResponseContext, BOOL *outNotifyDelegate, BOOL *outShouldRetry) {
-
-		if (
-		
-			([inResponseOrNil valueForKey:@"errors"] != [NSNull null]) || 
-			([[inResponseOrNil valueForKey:@"result.places"] isEqual:[NSNull null]]) || 
-			(![[inResponseOrNil valueForKey:@"result.places"] isKindOfClass:[NSArray class]])
-		 
-		) {
-		
-		//	Error #6 means that the location is not in Twitter’s database yet
-		
-			if ([(NSNumber *)[inUserInfo valueForKey:@"seekAlternativeGeolocationServices"] boolValue] == YES) {
-			
-				NSLog(@"FIXME: We’re going to ask for help from Google.");
-			
-			} else {
-		
-				inFailureCallback(inResponseOrNil, inResponseContext, outNotifyDelegate, outShouldRetry);
-				return;
-				
-			}
-		
-		}
-		
-		//	NSArray *places = [inResponseOrNil valueForKey:@"result.places"];
-		
-		NSLog(@"geo/reverse_geocode response: %@", inResponseOrNil);
-		
-	} failureHandler: ^ (NSDictionary *inResponseOrNil, NSDictionary *inResponseContext, BOOL *outNotifyDelegate, BOOL *outShouldRetry) {
 	
-		inFailureCallback(inResponseOrNil, inResponseContext, outNotifyDelegate, outShouldRetry);
-	
-	}];
-
 }
 
 
